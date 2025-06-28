1b3b18297bb55e3982bafaad2851f87ddf0f737b
chcom <chcom@DESKTOP-4BUL2DS>
2025-06-29 03:17:28 +0900

    2025 06 29 KSW

--- src/main/java/kr/or/ddit/resident/residentController/residentMyhouseController.java
+++ src/main/java/kr/or/ddit/resident/residentController/residentMyhouseController.java
@@ -1,8 +1,16 @@
 package kr.or.ddit.resident.residentController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/resident")
 public class residentMyhouseController {
 	
+	 @GetMapping("/myhouse")
+	    public String showMyHousePage() {
+	        return "resident/myHouse/myHouseMainpage";  
+	    }
+
 }
--- src/main/webapp/WEB-INF/fragments/rentalHeader.jsp
+++ src/main/webapp/WEB-INF/fragments/rentalHeader.jsp
@@ -25,7 +25,7 @@
               </a>
             </li>
          <li class="nav-item ">
-              <a class="nav-link" href="${pageContext.request.contextPath}/admin/pages/tables/bootstrap-tables.html">
+              <a class="nav-link" href="${pageContext.request.contextPath}/resident/myhouse">
                 <span class="font-large text-gray-800">마이하우스</span>
               </a>
             </li>
