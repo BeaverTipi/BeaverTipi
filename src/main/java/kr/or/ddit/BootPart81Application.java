6aba075ce3b2df7758e07b7662feff7b12b0f8e1
PC-16 <PC-16@DESKTOP-IHJ3V19>
2025-07-03 12:20:14 +0900

    2025-07-03 KAR
    암호화 작업

--- src/main/java/kr/or/ddit/BootPart81Application.java
+++ src/main/java/kr/or/ddit/BootPart81Application.java
@@ -3,7 +3,12 @@ package kr.or.ddit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySources;
 @EnableScheduling
+@EnableEncryptableProperties
 @SpringBootApplication
 public class BootPart81Application {
 
